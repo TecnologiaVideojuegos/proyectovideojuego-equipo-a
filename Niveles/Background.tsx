54a2429943bb7c541328a43fed4351c32070d456
MuradMehmed <61018938+MuradMehmed@users.noreply.github.com>
2020-05-03 10:34:35 +0200

    Añadido mapa de prueba

---
+++ Niveles/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Background 4" tilewidth="32" tileheight="32" tilecount="527" columns="31">
+ <image source="../Sprites/Obstaculos y paisaje/Background 1.jpg" width="1002" height="559"/>
+</tileset>
---
+++ Niveles/Suelo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Suelo" tilewidth="32" tileheight="32" tilecount="85" columns="17">
+ <image source="../Sprites/Obstaculos y paisaje/Suelo.png" width="575" height="160"/>
+</tileset>
